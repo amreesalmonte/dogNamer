d487de562f2c7699a49f4abfdb83008cf9aa04d3
amreesalmonte <amreesalmonte@gmail.com>
2020-02-05 18:07:15 -0700

    fixed dog movement

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
 function moveDog() {
     var dog = document.getElementById("dog");
+    var dogWidth = dog.clientWidth;
     var position = 0;
-
+    var change = 0;
     setInterval(function() {
-        rightEdge = window.innerWidth - 100; //100 is the size of the dog
-        if (position <= rightEdge) {
+        //variable keeps track when the dog should reset at start of page
+        rightEdge = window.innerWidth; //100 is the size of the dog
+        if (change == 0) {
             position++;
             dog.style.left = position + "px";
+            if (position == rightEdge) {
+                change = 1;
+            }
+        } else if (change == 1) {
+            position = -dogWidth;   //resets image back to left edge of screen
+            dog.style.left = position + "px";
+            change = 0;
         }
-    }, 1);
-
+    }, 30);
 }
 
 window.onload = function() {moveDog();};
