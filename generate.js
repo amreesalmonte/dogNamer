493d08b1bc550d7031fa1873a5f69da4be993a2b
amreesalmonte <amreesalmonte@gmail.com>
2020-02-24 16:23:26 -0700

    added more names

--- generate.js
+++ generate.js
@@ -1,7 +1,7 @@
 // this file contains all the functions that randomly chooses a name and title from a list
 
 function FemaleName() {
-    var names = ["anne", "cupcake", "apple"];
+    var names = ["cupcake", "apple", "daisy", "luna", "coco", "mimi", "lulu", "pearl", "ruby", "suki", "sasha", "mocha", "pepper", "pumpkin", "jo", "nana", "winnie"];
 
     var name = names[Math.floor(Math.random()* names.length)]; //randomly chooses a name from array
 
@@ -9,7 +9,7 @@ function FemaleName() {
 }
 
 function MaleName() {
-    var names = ["bob", "greg", "bowser"];
+    var names = ["bob", "greg", "bowser", "max", "charlie", "cooper", "buddy", "milo", "bear", "teddy", "jax", "finn", "jack", "moose", "loki", "mickey", "clipton", "bruno"];
 
     var name = names[Math.floor(Math.random()* names.length)]; //randomly chooses a name from array
 
@@ -17,7 +17,7 @@ function MaleName() {
 }
 
 function Titles() {
-    var titles = ["mayor", "officer", "doctor", "detective"];
+    var titles = ["mayor", "officer", "doctor", "detective", "chief", "your majesty", "your highness"];
 
     var title = titles[Math.floor(Math.random()* titles.length)]; //randomly chooses a name from array
 
